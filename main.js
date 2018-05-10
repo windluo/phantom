64e8010e212fffdf8a9a40f4853d6d5452357cfd
windluo <luo19302@163.com>
2018-05-10 16:53:41 +0800

    简单的抓取页面请求

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ var loadUrlTime = function (startTime) {
   return time;
 }
 
-var screenShot = function (page, name) {
+var screenShot = function (name) {
   // 截图页面
   // var time = new Date().getTime();
   var time = '';
   page.render('./images/' + name + time + '.png');
 }
 
-var getTitle = function (page) {
+var getTitle = function () {
   // 获取页面标题
   var title = page.evaluate(function() {
     return document.title;
@@ -26,13 +26,21 @@ var getTitle = function (page) {
 // page.onConsoleMessage = function(msg) {
 //   console.log('页面的console.log：' + msg);
 // };
+// // 简单的获取页面请求
+// page.onResourceRequested = function(request) {
+//   console.log('Request：' + JSON.stringify(request, undefined, 4));
+// };
+// // 简单的获取页面响应
+// page.onResourceReceived = function(response) {
+//   console.log('Receive：' + JSON.stringify(response, undefined, 4));
+// };
 
 var startTime = Date.now();
 page.open('http://www.baidu.com', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
     loadUrlTime(startTime);
-    // screenShot(page, 'baidu');
+    // screenShot('baidu');
     getTitle(page);
   } else {
     console.log('FAIL to load the address');
